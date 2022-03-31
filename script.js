fa9d5ef8a212ff2549cb0e732ff353114867bda8
Yan Gustavo <fekieh35@gmail.com>
2022-03-31 17:42:53 -0300

    Etapa 18 concluído

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const entrar = document.getElementById('loginBtn');
+const check = document.getElementById('agreement')
+const botao = document.getElementById('submit-btn')
 
 // Requisito 1
 
@@ -24,3 +26,13 @@ function contador() {
 }
 
 textArea.addEventListener('input', contador);
+
+//requisito 19
+function ligaBotao() {
+  botao.disabled = true
+if (check.checked) {
+  botao.disabled = false
+}
+}
+
+check.addEventListener('click', ligaBotao)
