f71b4924a05e1cb8f2f8d05c91fb0a765c8d7cd4
Yan Gustavo <fekieh35@gmail.com>
2022-03-31 15:33:58 -0300

    Etapa 3 concluída

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
+const entrar = document.getElementById('loginBtn')
+
+//Requisito 1
+entrar.addEventListener('click', validar)
+
+function validar() {
+    if (email.value === 'tryber@teste.com' & senha.value === '123456') {
+        alert('Olá, Tryber!')
+    } else alert('Email ou senha inválidos.')
+}
