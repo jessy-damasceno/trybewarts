4365c6857140c5efd3e666cf5e96f096a339ae17
Jessy Damasceno <jessy_bas@hotmail.com>
2022-04-01 00:54:07 -0300

    aplicando css styles

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const email = document.getElementById('email');
-const senha = document.getElementById('senha');
-const entrar = document.getElementById('loginBtn');
-const check = document.getElementById('agreement');
-const botao = document.getElementById('submit-btn');
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const entrar = document.getElementById("loginBtn");
+const check = document.getElementById("agreement");
+const botao = document.getElementById("submit-btn");
 
 // Requisito 1
 
 function validar() {
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert('Olá, Tryber!');
-  } else alert('Email ou senha inválidos.');
+  if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!");
+  } else alert("Email ou senha inválidos.");
 }
 
-entrar.addEventListener('click', validar);
+entrar.addEventListener("click", validar);
 
 // Requisito 20
 
-const textArea = document.querySelector('#textarea');
-const textoContador = document.querySelector('#counter');
+const textArea = document.querySelector("#textarea");
+const textoContador = document.querySelector("#counter");
 
 function contador() {
   let count = 500;
@@ -25,7 +25,7 @@ function contador() {
   textoContador.innerText = `Faltam ${count} caracteres`;
 }
 
-textArea.addEventListener('input', contador);
+textArea.addEventListener("input", contador);
 
 // Requisito 19
 function ligaBotao() {
@@ -35,22 +35,22 @@ function ligaBotao() {
   }
 }
 
-check.addEventListener('click', ligaBotao);
+check.addEventListener("click", ligaBotao);
 
 // Requisito 21   -----> https://www.javascripttutorial.net/javascript-dom/javascript-checkbox/ <-----
 function pegaNome() {
-  const name = document.querySelector('#input-name').value;
-  const lname = document.querySelector('#input-lastname').value;
+  const name = document.querySelector("#input-name").value;
+  const lname = document.querySelector("#input-lastname").value;
 
   return `Nome: ${name} ${lname}`;
 }
 
 function pegaEmail() {
-  return `Email: ${document.querySelector('#input-email').value}`;
+  return `Email: ${document.querySelector("#input-email").value}`;
 }
 
 function pegaCasa() {
-  return `Casa: ${document.querySelector('#house').value}`;
+  return `Casa: ${document.querySelector("#house").value}`;
 }
 
 function pegaFamilia() {
@@ -61,12 +61,12 @@ function pegaFamilia() {
 
 function pegaMaterias() {
   const materias = [];
-  const hofs = document.getElementById('hofs');
-  const jest = document.getElementById('jest');
-  const promises = document.getElementById('promises');
-  const react = document.getElementById('react');
-  const sql = document.getElementById('sql');
-  const python = document.getElementById('python');
+  const hofs = document.getElementById("hofs");
+  const jest = document.getElementById("jest");
+  const promises = document.getElementById("promises");
+  const react = document.getElementById("react");
+  const sql = document.getElementById("sql");
+  const python = document.getElementById("python");
 
   python.value = " Python";
   sql.value = " SQL";
@@ -76,37 +76,46 @@ function pegaMaterias() {
   hofs.value = " Hofs";
 
   document
-  .querySelectorAll('input[name="materias"]:checked')
-  .forEach((materia) => {
+    .querySelectorAll('input[name="materias"]:checked')
+    .forEach((materia) => {
       materias.push(materia.value);
     });
   return `Matérias: ${materias}`;
 }
 
 function pegaNota() {
-  return `Avaliação: ${document.querySelector('input[name="rate"]:checked').value}`;
+  return `Avaliação: ${
+    document.querySelector('input[name="rate"]:checked').value
+  }`;
 }
 
 function pegaObservacao() {
-  return `Observações: ${document.querySelector('#textarea').value}`;
+  return `Observações: ${document.querySelector("#textarea").value}`;
 }
 
- function apagaForm(event) {
-   const construcao = [pegaNome(), pegaEmail(), pegaCasa(), pegaFamilia(), pegaMaterias(), pegaNota(), pegaObservacao()];
-   const ol = document.createElement('ol');
+function apagaForm(event) {
+  const construcao = [
+    pegaNome(),
+    pegaEmail(),
+    pegaCasa(),
+    pegaFamilia(),
+    pegaMaterias(),
+    pegaNota(),
+    pegaObservacao(),
+  ];
+  const ol = document.createElement("ol");
 
-   event.preventDefault();
-   form.innerHTML = '';
-   form.appendChild(ol)
-   const buscaol = form.firstChild
-   for (let i = 0; i < construcao.length; i += 1) {
-     criaValor.innerText = construcao[i];
-     buscaol.appendChild(criaValor.cloneNode(true));
-     
-   }
+  event.preventDefault();
+  form.innerHTML = "";
+  form.appendChild(ol);
+  const buscaol = form.firstChild;
+  for (let i = 0; i < construcao.length; i += 1) {
+    criaValor.innerText = construcao[i];
+    buscaol.appendChild(criaValor.cloneNode(true));
+  }
 }
 
-const form = document.getElementById('evaluation-form');
-const criaValor = document.createElement('li')
+const form = document.getElementById("evaluation-form");
+const criaValor = document.createElement("li");
 
-botao.addEventListener('click', apagaForm)
+botao.addEventListener("click", apagaForm);
